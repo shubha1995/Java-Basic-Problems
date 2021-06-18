4c4309d8337a01f79d27b2230c6d913c25d51407
shubhasankar <ssbhaumikdmr@gmail.com>
2021-06-19 01:24:20 +0530

    Added: Utility Class

---
+++ Utility.java
@@ -0,0 +1,20 @@
+package javaPrograms;
+
+import java.util.Scanner;
+
+public class Utility {
+	private static final Scanner sc = new Scanner(System.in);
+	public static double getRandomDouble() {
+		return Math.random();
+	}
+	
+	public static int getUserSpecificInteger() {
+		return sc.nextInt();
+	}
+	public static char getUserSpecificChar() {
+		return sc.next( ).charAt(0);
+	}
+	public static double getUserSpecificDouble() {
+		return sc.nextDouble();
+	}
+}
